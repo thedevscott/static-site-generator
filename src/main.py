a4c1144b14039d6b7f8db55731de63e0191106ec
Omar Scott <thedevscott@gmail.com>
2025-05-12 18:05:24 -0400

    split node complete

--- src/main.py
+++ src/main.py
@@ -4,5 +4,6 @@ def main():
     text_node = tn.TextNode("The Begining", tn.TextType.NORMAL, "/")
     print(text_node)
 
+
 if __name__ == '__main__':
     main()
--- src/textnode.py
+++ src/textnode.py
@@ -35,3 +35,30 @@ def text_node_to_html_node(text_node):
     if text_node.type == TextType.IMAGE:
         return LeafNode(tag="img", value="", props={"src": text_node.url, "alt": text_node.text})
     raise ValueError(f"invalid text type: {text_node.type}")
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for old_node in old_nodes:
+        
+        if old_node.type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        
+        split_nodes = []
+        sections = old_node.text.split(delimiter)
+        
+        if len(sections) % 2 == 0:
+            raise ValueError("Invalid markdown. unclosed format.")
+
+        for i in range(len(sections)):
+            if sections[i] == "":
+                continue
+            if i % 2 == 0:
+                split_nodes.append(TextNode(text_in=sections[i], type_in=TextType.TEXT))
+            else:
+                split_nodes.append(TextNode(text_in=sections[i], type_in=text_type))
+        
+        new_nodes.extend(split_nodes)
+    
+    return new_nodes
+
